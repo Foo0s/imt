73df256cf4f7610ebf7ddd66aea211cdba2e0db3
Foo0s <62848145+Foo0s@users.noreply.github.com>
2023-11-10 22:50:44 +0300

    Update bd_work.py

--- bd_work.py
+++ bd_work.py
@@ -1,6 +1,6 @@
 import sqlite3 as sql
 
 def sert_info(height: float, weight: float, result: float):
-    with sql.connect("C:\\Users\\orapr\\OneDrive\\Рабочий стол\\Источники\\Тестики Python\\testbd.db") as database:
+    with sql.connect("testbd.db") as database:
         cursor = database.cursor()
         cursor.execute(f"INSERT INTO result (height, weight, result) VALUES {height, weight, result}")
